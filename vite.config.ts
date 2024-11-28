4b54e4e3fd6b769294d8be12f7201a0fbc1aee48
Alexander Strasser <alexander.strasser@protonmail.com>
2024-11-28 21:54:02 +0100

    chore: prepare Convex-test setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ export default defineConfig({
 	plugins: [sveltekit()],
 
 	test: {
-		includeSource: ['src/**/*.{js,ts}']
+		includeSource: ['src/**/*.{js,ts}'],
+		environmentMatchGlobs: [
+			// all tests in convex/ will run in edge-runtime
+			['convex/**', 'edge-runtime'],
+			// all other tests use node
+			['**', 'node']
+		],
+		server: { deps: { inline: ['convex-test'] } }
 	},
 	define: {
 		'import.meta.vitest': 'undefined'
