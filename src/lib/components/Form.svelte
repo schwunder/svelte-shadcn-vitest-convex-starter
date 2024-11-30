3bf9837f7d2a02b5a7358311523e924274fe5dd5
Alexander Strasser <alexander.strasser@protonmail.com>
2024-11-30 11:33:20 +0100

    feat(components): :tada: added a very simple form

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,14 @@
+<script>
+	let email = '';
+</script>
+
+<form>
+	<input
+		type="email"
+		bind:value={email}
+		class="form-input"
+		placeholder="Email"
+		aria-label="Form Submit Input"
+	/>
+	<button type="submit" class="form-button" aria-label="Form Submit Button">Submit</button>
+</form>
