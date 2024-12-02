fd0e80149e4361fd84bebc0f3767704434001202
Alexander Strasser <alexander.strasser@protonmail.com>
2024-12-02 14:49:07 +0100

    feat(components): :bulb: added lightswitch, default darkmode

---
+++ src/lib/components/LightSwitch.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$ui/button';
+</script>
+
+<Button onclick={toggleMode} variant="outline" size="icon">
+	<Sun />
+	<Moon />
+	<span class="sr-only">Toggle theme</span>
+</Button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { setupConvex } from 'convex-svelte';
+	import { ModeWatcher } from 'mode-watcher';
+	import LightSwitch from '$components/LightSwitch.svelte';
 	import '../app.css';
 
 	const { children } = $props();
 	setupConvex(PUBLIC_CONVEX_URL);
+
+	// Set dark mode by default
+	if (typeof document !== 'undefined') {
+		document.documentElement.classList.add('dark');
+		localStorage.setItem('mode-watcher-mode', 'dark');
+	}
 </script>
 
+<header>
+	<LightSwitch />
+</header>
+
+<ModeWatcher defaultMode="dark" />
 {@render children()}
